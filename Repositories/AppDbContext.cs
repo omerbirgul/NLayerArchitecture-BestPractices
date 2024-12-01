a9be06156ccb1d65305a0d0004cf630f56fe4eb0
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-12-01 18:19:47 +0300

    entity, dbContext and configuration added

---
+++ Repositories/AppDbContext.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Entities;
+
+namespace Repositories;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+
+
+    public DbSet<Product> Products { get; set; } = default!;
+}
