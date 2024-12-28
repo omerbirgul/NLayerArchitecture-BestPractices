f678402707a4e91c8f5fecebf05e6f250f5542ef
Ömer Faruk Birgül <omerbirgul45@gmail.com>
2024-12-29 01:54:05 +0300

    feat(Services): add FluentValidationFilter

--- App.API/Program.cs
+++ App.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Repositories.Extensions;
+using Services;
 using Services.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,7 +15,12 @@ builder.Services.AddRepositories(builder.Configuration).AddService(builder.Confi
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add<FluentValidationFilter>();
+    opt.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
